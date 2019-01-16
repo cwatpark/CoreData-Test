299e7525e404600e72afb4bf9836ff288002651e
Ice <ice@macbook-pro-4.local>
2019-01-16 14:51:26 +0700

    Initial Commit

--- CoreDataX2/ViewController.swift
+++ CoreDataX2/ViewController.swift
@@ -1,20 +1,42 @@
-//
-//  ViewController.swift
-//  CoreDataX2
-//
-//  Created by Ice on 16/1/2562 BE.
-//  Copyright © 2562 Ice. All rights reserved.
-//
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var txtfirstname: UITextField!
+    @IBOutlet weak var txtField: UITextField!
+    @IBOutlet weak var txtLabel: UILabel!
+    
+    var itemArray = [NameEn]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
     }
 
-
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        let newItem  = NameEn(context: self.context)
+        newItem.firstname = txtfirstname.text
+        newItem.lastname = txtField.text
+        
+        self.itemArray.append(newItem)
+        do{
+            try context.save()
+            print("Save Context Successful")
+        }catch{
+            print("Error Cant save Context \(error)")
+        }
+        
+    }
+    
 }
 
