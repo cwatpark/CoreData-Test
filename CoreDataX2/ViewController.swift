0f3f4884b6eba26b16d1815f8e30cbe25b680190
Ice <ice@macbook-pro-4.local>
2019-01-16 14:32:59 +0700

    Initial Commit

---
+++ CoreDataX2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataX2
+//
+//  Created by Ice on 16/1/2562 BE.
+//  Copyright © 2562 Ice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
